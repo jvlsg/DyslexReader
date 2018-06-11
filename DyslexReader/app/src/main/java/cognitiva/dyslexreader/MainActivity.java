636b872f14ba543a04cdcd544d2061a9a346f4d9
Unknown <jvguima@yahoo.com.br>
2018-06-11 17:33:33 -0300

    Began Main Activity

---
+++ DyslexReader/app/src/main/java/cognitiva/dyslexreader/MainActivity.java
@@ -0,0 +1,67 @@
+package cognitiva.dyslexreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Button to ReaderActivity
+    Button btnReader;
+    Button btnFile;
+    Button btnPaste;
+    Button btnCamera;
+    Button btnSettings;
+
+    TextView tvPreview;
+
+    /***
+     * TODO: Declaration of Parser Classes
+     *
+     * ImageParser imageParser
+     * HTTPParser httpParser
+     * EPUBParser epubParser
+     * PDFParser pdfParser
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /***
+     * Button Callback for btnReader
+     */
+    public void onClickReader(){
+
+    }
+
+    /***
+     * Button Callback for btnFile
+     */
+    public void onClickFile(){
+
+    }
+
+    /***
+     * Button Callback for btnPaste
+     */
+    public void onClickPaste(){
+
+    }
+
+    /**
+     *
+     */
+    public void onClickCamera(){}
+
+    /***
+     * Button Callback for btnSettings
+     */
+    public void onClickSettings(){
+
+    }
+}
