87cb32908f0b55b69054ca0f98a8da75dcf2b088
Unknown <jvguima@yahoo.com.br>
2018-06-13 11:15:43 -0300

    Continued preferences
    *Adicionou resources para keys
    TODO: Mergir com branch Kenji - integrar códigos e atualizar Main.

--- DyslexReader/app/src/main/java/cognitiva/dyslexreader/SettingsActivity.java
+++ DyslexReader/app/src/main/java/cognitiva/dyslexreader/SettingsActivity.java
@@ -1,19 +1,35 @@
 package cognitiva.dyslexreader;
 
+import android.preference.SwitchPreference;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private SwitchPreference switchWhiteNoise;
+    private SwitchPreference switchFistLastColors;
+
+
+    //TODO: HOLDTIME
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+
+        /**
+         * Botão para retorno ao menu Principal
+         */
         ActionBar actionBar = this.getSupportActionBar();
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        /**
+         *
+         */
+        //switchWhiteNoise = (SwitchPreference) findPr
     }
 }
