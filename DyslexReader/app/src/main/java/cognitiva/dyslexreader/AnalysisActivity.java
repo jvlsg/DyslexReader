632433f06dad828bcd85e1c1f62c4b3d3158ebfc
Unknown <jvguima@yahoo.com.br>
2018-06-14 15:55:39 -0300

    Began Implementing Analysis Activity
    +Analysis Portrait UI done
    TODO:  Landscape UI, DataBinding, HTTP Connection?

--- DyslexReader/app/src/main/java/cognitiva/dyslexreader/AnalysisActivity.java
+++ DyslexReader/app/src/main/java/cognitiva/dyslexreader/AnalysisActivity.java
@@ -1,5 +1,7 @@
 package cognitiva.dyslexreader;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class AnalysisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis);
+
+        /*Intent intent = getIntent();
+        String wordToAnalyze = intent.getExtras().getString(getString(R.string.wordToAnalyzeKey));
+        */
+        //wordToAnalyze = "Duck";
+
+        /**
+         * Botão para retorno à Reader ACtivity
+         */
+        ActionBar actionBar = this.getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
