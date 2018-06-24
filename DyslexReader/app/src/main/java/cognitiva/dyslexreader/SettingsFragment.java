dc0c237777a64e34540184456201285fb8d818d6
Unknown <jvguima@yahoo.com.br>
2018-06-24 17:01:13 -0300

    Added Color customization for custom theme

--- DyslexReader/app/src/main/java/cognitiva/dyslexreader/SettingsFragment.java
+++ DyslexReader/app/src/main/java/cognitiva/dyslexreader/SettingsFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.ColorInt;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.widget.Toast;
+import com.thebluealliance.spectrum.SpectrumPreferenceCompat;
 
 import com.thebluealliance.spectrum.SpectrumDialog;
 
@@ -18,32 +19,11 @@ public class SettingsFragment extends PreferenceFragmentCompat{
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_dyslex);
 
-        findPreference(getString(R.string.themeCustomBackgroundKey)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override public boolean onPreferenceClick(Preference preference) {
-                showDialog2();
-                return true;
-            }
-        });
-
     }
 
-
-
-    private void showDialog2() {
-        new SpectrumDialog.Builder(getContext())
-                .setColors(R.array.colorsForCustomTheme)
-                .setSelectedColorRes(R.color.md_blue_500)
-                .setDismissOnColorSelected(false)
-                .setOutlineWidth(2)
-                .setOnColorSelectedListener(new SpectrumDialog.OnColorSelectedListener() {
-                    @Override public void onColorSelected(boolean positiveResult, @ColorInt int color) {
-                        if (positiveResult) {
-                            Toast.makeText(getContext(), "Color selected: #" + Integer.toHexString(color).toUpperCase(), Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getContext(), "Dialog cancelled", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }).build().show(getFragmentManager(), "dialog_demo_2");
+    @Override public void onDisplayPreferenceDialog(Preference preference) {
+        if (!SpectrumPreferenceCompat.onDisplayPreferenceDialog(preference, this)) {
+            super.onDisplayPreferenceDialog(preference);
+        }
     }
-
 }
