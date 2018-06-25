a9daaacfee0ad61b4d5c2c9709091d6e4d46eae9
Otavio <otavio.luis.aguiar@gmail.com>
2018-06-25 04:41:31 -0300

    AnalysisActivity main functionality done

--- DyslexReader/app/src/main/java/cognitiva/dyslexreader/AnalysisActivity.java
+++ DyslexReader/app/src/main/java/cognitiva/dyslexreader/AnalysisActivity.java
@@ -1,19 +1,43 @@
 package cognitiva.dyslexreader;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
 import android.widget.Toast;
 import android.content.SharedPreferences;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class AnalysisActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
@@ -22,7 +46,7 @@ public class AnalysisActivity extends AppCompatActivity implements SharedPrefere
     TextView tvWord;
     TextView tvMeaning;
     TextView tvPhonetics;
-    Button btnPrinunciation;
+    Button btnPronunciation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +56,9 @@ public class AnalysisActivity extends AppCompatActivity implements SharedPrefere
 
         tvWord = (TextView) findViewById(R.id.tvSyllable);
         tvWord.setMovementMethod(new ScrollingMovementMethod());
+        tvMeaning = (TextView) findViewById(R.id.tvDefinition);
+        tvMeaning.setMovementMethod(new ScrollingMovementMethod());
+        btnPronunciation = findViewById(R.id.btnPronunciation);
 
         setBackground();
 
@@ -141,6 +168,306 @@ public class AnalysisActivity extends AppCompatActivity implements SharedPrefere
             recreate();
         }
 
+    }
+
+    public class FetchWord extends AsyncTask<String, Void, Word> {
+
+        private static final String API_KEY = "33f1899bae9a7328fd0020ed3710587667a3c3fa92cade914";
+
+        @Override
+        protected Word doInBackground(String... strings) {
+
+            ArrayList<Pair<String, String>> definitions = getDefinitions(strings[0]);
+            Pair<String[], Integer> hyphenation = getHyphenation(strings[0]);
+            String url = getAudioURL(strings[0]);
+            Word word = null;
+
+            if ((url != null)
+                    && (downloadAudioFile(strings[0], url))
+                    && (definitions != null)
+                    && (hyphenation != null)) {
+                word = new Word(strings[0].toLowerCase(), hyphenation, definitions);
+            }
+
+            return word;
+        }
+
+        @Override
+        protected void onPostExecute(final Word word) {
+            super.onPostExecute(word);
+            if (word == null) {
+                Log.d("Word", "word null");
+                return;
+            }
+
+            //Concatena silabas com espaco entre elas
+            Pair<String[], Integer> hyphenation = word.getHyphenation();
+            StringBuilder builderHyph = new StringBuilder();
+            builderHyph.append(hyphenation.first[0]);
+            String sAux;
+            for (int i = 1; i < hyphenation.first.length; ++i) {
+                sAux = " · " + hyphenation.first[i];
+                builderHyph.append(sAux);
+            }
+
+            tvWord.setText(builderHyph.toString());
+
+            //Concatenas todas as definicoes em 1 string
+            ArrayList<Pair<String, String>> definitions = word.getDefinitions();
+            StringBuilder builderDef = new StringBuilder();
+            String type, def;
+            for (int i = 0; i < definitions.size(); ++i) {
+                type = definitions.get(i).first;
+                def = definitions.get(i).second;
+                sAux = (i+1)+". ("+type+")\n"+def+"\n";
+                builderDef.append(sAux);
+            }
+
+            tvMeaning.setText(builderDef.toString());
+
+            btnPronunciation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MediaPlayer mp = MediaPlayer.create(v.getContext(), Uri.parse(Environment.getExternalStorageDirectory().getPath()+ "/dictionary/"+word.getWord()+".mp3"));
+                    mp.setLooping(false);
+                    mp.start();
+                }
+            });
+        }
+
+        private ArrayList<Pair <String, String>> getDefinitions(String word) {
+            Response response = null;
+
+            String url = "https://api.wordnik.com/v4/word.json/" + word.toLowerCase() +
+                    "/definitions?limit=10&includeRelated=false&sourceDictionaries=wiktionary" +
+                    "&useCanonical=false&includeTags=false&api_key=" + API_KEY;
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder().url(url).build();
+
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (response != null) {
+                try {
+                    String json = response.body().string();
+                    Log.d("FetchWord-definition", json);
+                    return getDefinitionsJSON(json);
+                } catch (IOException e) {
+                    Log.d("FetchWord-definition", "erro");
+                    e.printStackTrace();
+                }
+            } else {
+                Log.d("FetchWord-definition", "erro");
+            }
+
+            return null;
+        }
+
+        private ArrayList<Pair <String, String>> getDefinitionsJSON(String jsonString) {
+            JSONArray json = null;
+            JSONObject def = null;
+            int arraySize = 0;
+            ArrayList<Pair <String, String>> definitions = new ArrayList<>();
+
+            try {
+                json = new JSONArray(jsonString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            if (json != null) {
+                arraySize = json.length();
+                for (int i = 0; i < arraySize; ++i) {
+                    try {
+                        def = json.getJSONObject(i);
+                        definitions.add(Pair.create(def.getString("partOfSpeech"), def.getString("text")));
+                        Log.d("FetchWord-partOfSpeech", def.getString("partOfSpeech"));
+                        Log.d("FetchWord-text", def.getString("text"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+
+                }
+            } else {
+                Log.d("FetchWord-definition", "erro json");
+                return null;
+            }
+
+            return definitions;
+        }
+
+        private Pair<String[], Integer> getHyphenation(String word) {
+            Response response = null;
+
+            String url = "https://api.wordnik.com/v4/word.json/" + word.toLowerCase() +
+                    "/hyphenation?useCanonical=false&limit=50&api_key=" + API_KEY;
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder().url(url).build();
+
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (response != null) {
+                try {
+                    String json = response.body().string();
+                    Log.d("FetchWord-hyphenation", json);
+                    return getHyphenationJSON(json);
+                } catch (IOException e) {
+                    Log.d("FetchWord-hyphenation", "erro");
+                    e.printStackTrace();
+                }
+            } else {
+                Log.d("FetchWord-hyphenation", "erro");
+            }
+
+            return null;
+        }
+
+        private Pair<String[], Integer> getHyphenationJSON(String jsonString) {
+            JSONArray json = null;
+            JSONObject sy = null;
+            int arraySize = 0;
+            Integer stress = 0;
+            String[] syllable = null;
+
+            try {
+                json = new JSONArray(jsonString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            if (json != null) {
+                arraySize = json.length();
+                syllable = new String[arraySize];
+                for (int i = 0; i < arraySize; ++i) {
+                    try {
+                        sy = json.getJSONObject(i);
+                        syllable[i] = sy.getString("text");
+                        if (sy.optString("type") == "stress") {
+                            stress = i;
+                        }
+                        Log.d("hyphenation-syllable", syllable[i]);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
+                Log.d("hyphenation-stress", String.valueOf(stress));
+            } else {
+                Log.d("FetchWord-hyphenation", "erro json");
+                return null;
+            }
+
+            return Pair.create(syllable, stress);
+        }
+
+        private String getAudioURL(String word) {
+            Response response = null;
+
+            String url = "https://api.wordnik.com/v4/word.json/" + word.toLowerCase() +
+                    "/audio?useCanonical=false&limit=1&api_key=" + API_KEY;
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder().url(url).build();
+
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (response != null) {
+                try {
+                    String json = response.body().string();
+                    Log.d("FetchWord-audio", json);
+                    return getAudioUrlJson(json);
+                } catch (IOException e) {
+                    Log.d("FetchWord-audio", "erro");
+                    e.printStackTrace();
+                }
+            } else {
+                Log.d("FetchWord-audio", "erro");
+            }
+
+            return null;
+        }
+
+        private String getAudioUrlJson(String jsonString) {
+            JSONArray json = null;
+            JSONObject def = null;
+            String url;
+
+            try {
+                json = new JSONArray(jsonString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            if (json != null) {
+
+                try {
+                    def = json.getJSONObject(0);
+                    url = def.getString("fileUrl");
+                    Log.d("FetchWord-audioURL", url);
+                } catch (JSONException e) {
+                    Log.d("FetchWord-audioURL", "erro json");
+                    e.printStackTrace();
+                    return null;
+                }
+
+            } else {
+                Log.d("FetchWord-audioURL", "erro json");
+                return null;
+            }
+
+            return url;
+        }
+
+        private boolean downloadAudioFile(String word, String urlString) {
+            int count;
+            try {
+                URL url = new URL(urlString);
+                URLConnection conection = url.openConnection();
+                conection.connect();
+
+                InputStream input = new BufferedInputStream(url.openStream());
+                File folder = new File(Environment.getExternalStorageDirectory()+"/dictionary/");
+                Log.d("folderexists" , ""+folder.exists());
+                if (!folder.exists()) Log.d("mkdirs" , ""+folder.mkdirs());
+                File file = new File(folder, word.toLowerCase()+".mp3");
+                FileOutputStream output = new FileOutputStream(file);
+
+                byte data[] = new byte[1024];
+
+                while ((count = input.read(data)) != -1) {
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+                output.close();
+                input.close();
+                Log.d("FetchWord-audioFile", "arquivo mp3 baixado");
+                return true;
+            } catch (Exception e) {
+                Log.d("FetchWord-audioFile", "erro download");
+                e.printStackTrace();
+            }
+
+            return false;
+        }
     }
 
 
